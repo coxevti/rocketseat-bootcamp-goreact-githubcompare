3b019ee8a100a4fed38ece2f0b8e224ccea1a66d
Valdir Coxev <seti1@zornimat.com.br>
2019-03-07 17:20:54 -0400

    Aula: Estilizando lista de repos

--- src/components/compareList/index.js
+++ src/components/compareList/index.js
@@ -1,31 +1,85 @@
 import React from 'react';
 
+import { Container, Repository } from './style';
+
 const CompareList = () => (
-  <div>
-    <header>
-      <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-      <strong>react</strong>
-      <small>facebook</small>
-    </header>
-    <ul>
-      <li>
-        56,234
-        <small>starts</small>
-      </li>
-      <li>
-        56,234
-        <small>forks</small>
-      </li>
-      <li>
-        56,234
-        <small>issues</small>
-      </li>
-      <li>
-        3 days ago
-        <small>last commit</small>
-      </li>
-    </ul>
-  </div>
+  <Container>
+    <Repository>
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
+        <strong>react</strong>
+        <small>facebook</small>
+      </header>
+      <ul>
+        <li>
+          56,234
+          <small>starts</small>
+        </li>
+        <li>
+          56,234
+          <small>forks</small>
+        </li>
+        <li>
+          56,234
+          <small>issues</small>
+        </li>
+        <li>
+          3 days ago
+          <small>last commit</small>
+        </li>
+      </ul>
+    </Repository>
+    <Repository>
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
+        <strong>react</strong>
+        <small>facebook</small>
+      </header>
+      <ul>
+        <li>
+          56,234
+          <small>starts</small>
+        </li>
+        <li>
+          56,234
+          <small>forks</small>
+        </li>
+        <li>
+          56,234
+          <small>issues</small>
+        </li>
+        <li>
+          3 days ago
+          <small>last commit</small>
+        </li>
+      </ul>
+    </Repository>
+    <Repository>
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
+        <strong>react</strong>
+        <small>facebook</small>
+      </header>
+      <ul>
+        <li>
+          56,234
+          <small>starts</small>
+        </li>
+        <li>
+          56,234
+          <small>forks</small>
+        </li>
+        <li>
+          56,234
+          <small>issues</small>
+        </li>
+        <li>
+          3 days ago
+          <small>last commit</small>
+        </li>
+      </ul>
+    </Repository>
+  </Container>
 );
 
 export default CompareList;
---
+++ src/components/compareList/style.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 50px;
+`;
+
+export const Repository = styled.div`
+  @import url('https://fonts.googleapis.com/css?family=PT+Sans|Roboto');
+  width: 250px;
+  background: #fff;
+  border-radius: 3px;
+  display: flex;
+  flex-direction: column;
+  margin: 0 10px;
+  header {
+    padding: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    img {
+      width: 100px;
+      border-radius: 50%;
+    }
+    strong {
+      font-size: 26px;
+      margin-top: 10px;
+      text-transform: capitalize;
+      font-family: 'Roboto', sans-serif;
+    }
+    small {
+      font-size: 14px;
+      color: #666;
+    }
+  }
+  ul {
+    list-style: none;
+    li {
+      font-weight: bold;
+      padding: 12px 20px;
+      font-family: 'PT Sans', sans-serif;
+      small {
+        font-weight: normal;
+        font-size: 12px;
+        color: #999;
+        font-style: italic;
+        margin-left: 5px;
+      }
+      &:nth-child(2n -1) {
+        background: #f5f5f5;
+      }
+    }
+  }
+`;
